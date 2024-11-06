affcd7b948f0272365525b97a2af57a5f3cb22dc
jankarong <jankarong@gmail.com>
2024-11-06 10:40:33 -0800

    Delete videoplayer js

--- js/videoPlay.js
+++
@@ -1,4 +0,0 @@
-document.querySelectorAll('.swiper-slide video').forEach(video => {
-    video.addEventListener('mouseenter', () => video.play());
-    video.addEventListener('mouseleave', () => video.pause());
-});
